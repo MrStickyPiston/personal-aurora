6037857138930d7189cad7164c89415087aa676c
sticky <mr.sticky.piston@gmail.com>
2024-11-09 11:58:52 +0100

    replace code with codium

--- build.sh
+++ build.sh
@@ -12,11 +12,13 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-# rpm-ostree install screen
+# Remove vscode
+rpm-ostree override remove code
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# install vscodium
+rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" > /etc/yum.repos.d/vscodium.repo
+rpm-ostree install codium
 
 #### Example for enabling a System Unit File
 
