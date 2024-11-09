012fc8598be812c708c10e2864994285d45ea3e8
sticky <mr.sticky.piston@gmail.com>
2024-11-09 15:21:30 +0100

    switch browser from firefox to librewolf

--- build.sh
+++ build.sh
@@ -12,4 +12,7 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+flatpak remove org.mozilla.firefox
+flatpak install io.gitlab.librewolf-community
+
 #### Systemd services
